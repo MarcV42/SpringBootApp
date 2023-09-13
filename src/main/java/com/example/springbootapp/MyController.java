1a283faed7b9e3867d01deaae264253475260d4c
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-14 01:59:06 +0200

    Working with Postman2

--- src/main/java/com/example/springbootapp/MyController.java
+++ src/main/java/com/example/springbootapp/MyController.java
@@ -13,17 +13,23 @@ public class MyController {
 
     private final List<Message> messages = new ArrayList<>();
 
+
+    // http://localhost:8080/api/hello
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, Marc!";
     }
 
     @GetMapping("/hello/?{name}")
     public String sayHello(@PathVariable String name) {
         return "Hello, " + name + "!";
+
+        // for this above use:
+        // http://localhost:8080/api/hello/{boss
+        // in Postman as Get request
     }
 
-    //!!!!!
+    //!!!!! THis is the good json Post & get tool:
     @PostMapping("/messages")
     public Message addMessage(@RequestBody Message message) {
         messages.add(message);
@@ -43,5 +49,13 @@ public class MyController {
 
     }
 
+
+    //http://localhost:8080/api/{{12345}}
+    @GetMapping("/{id}")
+    public String getHelloPathVariable(@PathVariable String id) {
+        System.out.println("PathVariable: " + id);
+        return "Hello: " + id;
+
+    }
 }
 
